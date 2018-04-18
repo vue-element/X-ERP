ec8208ae795cb2a540be740c26260c0ded36ac04
zeroLynn123 <838327553@qq.com>
2018-04-18 16:24:08 +0800

    1

--- src/views/cost/storageManage/inbound/components/add.vue
+++ src/views/cost/storageManage/inbound/components/add.vue
@@ -268,11 +268,6 @@ export default {
       categoryList: [],
       supplyList: [],
       contractInfoList: [],
-<<<<<<< HEAD
-      container: true,
-      printTable: false,
-=======
->>>>>>> c037d8a0550705255d0c29bab2e9bb822f8021af
       inboundId: '',
       rules: {},
       container: true,
